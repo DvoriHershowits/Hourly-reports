6da838cccfaabf5d6d4670b9a360dedb245100b6
DvoriHershowits <d0548432970@gmail.com>
2025-05-25 01:16:08 +0300

    update port

--- backend/index.js
+++ backend/index.js
@@ -88,6 +88,6 @@ createCrudRoutes('Projects')
 createCrudRoutes('Employees')
 createCrudRoutes('HourlyReports')
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
+  });
